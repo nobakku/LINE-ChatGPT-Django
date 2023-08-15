f66ee1ba939a347937fd52e47a9be53ee6cb4fd7
nobata <ahirururu122@gmail.com>
2023-08-15 18:30:23 +0900

    DjangoとLINE Messaging API の連携完了   ngrokで動作確認済み

--- linebot_project/linebot_app/views.py
+++ linebot_project/linebot_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-# Create your views here.
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    signature = request.headers['X-Line-Signature']
+    body = request.body.decode('utf-8')
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=200)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)  # ここでGPT-3との連携を行う
+    )
+
+
+
+def index(request):
+    return HttpResponse("Hello, LINE bot!")
