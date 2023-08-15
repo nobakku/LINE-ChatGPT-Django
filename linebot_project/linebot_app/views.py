eaa34cb64a36885a1a3b29e56fd74cc2e705166d
nobata <ahirururu122@gmail.com>
2023-08-15 22:13:29 +0900

    LINEでGPTとチャット成功

--- linebot_project/linebot_app/views.py
+++ linebot_project/linebot_app/views.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
+from linebot_project.settings import OPENAI_API_KEY
 
-
+# 環境変数の設定
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
-
+openai.api_key = OPENAI_API_KEY
 
 @csrf_exempt
 def callback(request):
@@ -22,11 +24,22 @@ def callback(request):
 
     return HttpResponse(status=200)
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # GPT-3を使用してレスポンスを生成
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=event.message.text,
+      max_tokens=150,
+      temperature=0
+    )
+    response_text = response.choices[0].text.strip()
+
+    # LINE botがレスポンスを返す
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)  # ここでGPT-3との連携を行う
+        TextSendMessage(text=response_text)
     )
 
 
